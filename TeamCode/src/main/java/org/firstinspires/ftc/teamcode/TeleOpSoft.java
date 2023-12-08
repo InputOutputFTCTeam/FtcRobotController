55949e685a72ae707463240a1459849d4cafa671
Input/Output <ftc17438@gmail.com>
2023-12-08 20:09:28 +0300

    TeleOpSoft update new: cr servo control angle servo control. must be modified

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpSoft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpSoft.java
@@ -10,7 +10,7 @@ import com.qualcomm.robotcore.hardware.Servo;
 @TeleOp(name="TeleOp")
 public class TeleOpSoft extends LinearOpMode {
     public DcMotor TL, TR, BL, BR;
-    public Servo Servo1, Servo2; // верт (15, -90), гориз (-15, 60)
+    public Servo Servo1; // верт (15, -90), гориз (-15, 60)
     public CRServo CR;
     double tl, tr, bl, br;
 
@@ -22,7 +22,7 @@ public class TeleOpSoft extends LinearOpMode {
         BR = hardwareMap.dcMotor.get("rightRear");
 
         Servo1 = hardwareMap.servo.get("servo1");  //
-        Servo2 = hardwareMap.servo.get("servo2");  //
+        //Servo2 = hardwareMap.servo.get("servo2");  //
         CR = hardwareMap.crservo.get("cr");  //
 
         TL.setDirection(DcMotorSimple.Direction.FORWARD);
@@ -35,8 +35,7 @@ public class TeleOpSoft extends LinearOpMode {
         BL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         BR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        Servo1.setPosition(0.7);
-        Servo2.setPosition(0);
+        Servo1.setPosition(0.667);
 
         telemetry.addLine("Ready to start");
         waitForStart();
@@ -47,23 +46,22 @@ public class TeleOpSoft extends LinearOpMode {
                     gamepad1.right_trigger - gamepad1.left_trigger,
                     1
             );
-            //ServoMove(gamepad2.right_stick_x, gamepad2.right_stick_y, 100);
-            ServoMove(gamepad2.left_stick_x, gamepad2.right_stick_y + 0.70, 1);
-//            if (gamepad2.dpad_up) {
-//                Servo1.setPosition(Servo1.getPosition() + 0.05);
-//            }
-//            if (gamepad2.dpad_down) {
-//                Servo1.setPosition(Servo1.getPosition() - 0.05);
-//            }
-//            if (gamepad2.dpad_left) {
-//                Servo1.setPosition(Servo2.getPosition() + 0.05);
-//            }
-//            if (gamepad2.dpad_right) {
-//                Servo1.setPosition(Servo2.getPosition() - 0.05);
-//            }
+
+            if (gamepad2.dpad_up) {
+                Servo1.setPosition(0.667);
+            }
+            if (gamepad2.dpad_right) {      //поставить соответствующие кнопки
+                Servo1.setPosition(0.167);  //поменять на соответствующее горизонтальному положению (0 - вертикаль, (0.667)* - опущено до пола) *-настроить
+            }
+            if (gamepad2.dpad_down) {
+                Servo1.setPosition(0);
+            }
+
+            CR.setPower((gamepad2.right_trigger - gamepad2.left_trigger));
+
             CR.setPower(gamepad2.right_trigger - gamepad2.left_trigger);
             telemetry.addData("servo1", Servo1.getPosition());
-            telemetry.addData("servo2", Servo2.getPosition());
+            //telemetry.addData("servo2", Servo2.getPosition());
             telemetry.update();
         }
     }
@@ -81,6 +79,7 @@ public class TeleOpSoft extends LinearOpMode {
         BL.setPower(bl);
         BR.setPower(br);
     }
+    /*
     public void ServoMove(double x, double y, int k) {
 //        if (-0.5 <= Servo1.getPosition() && Servo1.getPosition() <= 0.15) {
 //            Servo1.setPosition(Servo1.getPosition() + (y / k));
@@ -92,5 +91,5 @@ public class TeleOpSoft extends LinearOpMode {
 //        Servo2.setPosition(Servo2.getPosition() + (x / k));
         Servo1.setPosition(y);
         Servo2.setPosition(x);
-    }
+    }*/
 }
