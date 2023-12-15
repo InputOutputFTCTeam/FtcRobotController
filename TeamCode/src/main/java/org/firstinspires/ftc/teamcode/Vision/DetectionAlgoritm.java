050242a95db39e233646bf867a35f3d56cd5746e
nelturion <aandreyuk2001@gmail.com>
2023-12-15 20:05:06 +0300

    Update merged from 'master' into 'Vision' also small add into 'DetectionAlgoritm.java'

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/DetectionAlgoritm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/DetectionAlgoritm.java
@@ -1,13 +1,17 @@
 package org.firstinspires.ftc.teamcode.Vision;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.SkyStoneAlgoritm;
 
 @Disabled
 @TeleOp(name = "Detection", group = "Test")
-public class DetectionAlgoritm {
-    SkyStoneAlgoritm detector = new SkyStoneAlgoritm();///
-    //надо додумать, доработать. времени не хватает...
+public class DetectionAlgoritm extends LinearOpMode {
+
+    @Override
+    public void runOpMode(){
+
+    }
 }
