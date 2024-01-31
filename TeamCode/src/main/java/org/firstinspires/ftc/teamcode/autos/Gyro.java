356a7563705bc7e1c9bb0b825d5eaa444307ef80
nelturion <aandreyuk2001@gmail.com>
2024-01-31 19:16:36 +0300

    29.01 imu testing something with vision

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/Gyro.java
@@ -0,0 +1,66 @@
+package org.firstinspires.ftc.teamcode.autos;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.robotcore.external.Func;
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+import java.util.Locale;
+
+@Autonomous(name = "Gyro monitoring")
+public class Gyro extends LinearOpMode {
+
+    BNO055IMU imu;
+    Orientation angles;
+
+    public void runOpMode() {
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+
+        waitForStart();
+
+        while (opModeIsActive()){
+
+            telemetry.addData("Current headings is", getHeadingString());
+            telemetry.addData("Current double heading is", g)
+            telemetry.update();
+        }
+    }
+    public double getHeadingDouble() {
+
+        angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        return angles.firstAngle;
+    }
+    public String getHeadingString() {
+            angles   = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+            return formatAngle(angles.angleUnit, angles.firstAngle);
+    }
+
+    String formatAngle(AngleUnit angleUnit, double angle) {
+        return formatDegrees(AngleUnit.DEGREES.fromUnit(angleUnit, angle));
+    }
+
+    String formatDegrees(double degrees){
+        return String.format(Locale.getDefault(), "%.1f", AngleUnit.DEGREES.normalize(degrees));
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/ColorSensorTele.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.teleops;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "TideColor")
+public class ColorSensorTele extends LinearOpMode {
+    //создать экземпляр (объект) класса OurColor
+    @Override
+    public void runOpMode(){
+        //инициализировать объект с помощью метода init(  параметром hardwareMap )
+
+        waitForStart();
+        while(opModeIsActive()){
+            //выполнять функцию getColoredLine()
+            //записывать значение этой функции в телеметрию
+
+        }
+    }
+}
