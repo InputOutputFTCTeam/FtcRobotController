e6b3c93c9203352ceb725dadb24795ab5d93e525
nelturion <aandreyuk2001@gmail.com>
2024-02-08 16:33:36 +0300

    начали работу с автономом roadrunner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/AutoBlueTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/AutoBlueTest.java
@@ -43,13 +43,13 @@ import org.openftc.easyopencv.OpenCvCameraRotation;
 @Autonomous(name = "AutoBlueTest", group = "Actul")
 
 public class AutoBlueTest extends LinearOpMode{
-    DcMotor TR, TL, BR, BL, Intake, Lift;
+    DcMotor TR, TL, BR, BL;
 
     Servo servobox, lohotronMain, lohotron, zahvat, drop2;
 
-    protected Recognition recognition;
 
-    OpenCvCamera webcam;
+
+
 
     double INTAKE_SPEED = 0.7;
 
@@ -76,37 +76,10 @@ public class AutoBlueTest extends LinearOpMode{
     @Override
 
     public void runOpMode() {
-        recognition = new Recognition();
-        recognition.getAnalysis();
-        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
-        webcam.openCameraDevice();
-        webcam.setPipeline(recognition);
-        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
-        {
-            @Override
-            public void onOpened()
-            {
-                webcam.startStreaming(1920,1080, OpenCvCameraRotation.UPRIGHT); //поменять ориентацию камеры  SIDEWAYS_LEFT
-            }
-
-            @Override
-            public void onError(int errorCode)
-            {
-                /*
-                 * This will be called if the camera could not be opened
-                 */
-            }
-        });
-
         TL = hardwareMap.dcMotor.get("leftFront");
         TR = hardwareMap.dcMotor.get("rightFront");
         BL = hardwareMap.dcMotor.get("leftRear");
         BR = hardwareMap.dcMotor.get("rightRear");
-        Intake = hardwareMap.dcMotor.get("intake");
-        Lift = hardwareMap.dcMotor.get("lift");
-
-
 
         servobox = hardwareMap.servo.get("servobox");
         lohotronMain = hardwareMap.servo.get("lohotronMain");
@@ -120,41 +93,18 @@ public class AutoBlueTest extends LinearOpMode{
         TR.setDirection(DcMotorSimple.Direction.REVERSE);
         BL.setDirection(DcMotorSimple.Direction.FORWARD);
         BR.setDirection(DcMotorSimple.Direction.REVERSE);
-        Intake.setDirection(DcMotorSimple.Direction.FORWARD);
-        Lift.setDirection(DcMotorSimple.Direction.FORWARD);
-
-
 
         TL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         TR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         BL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         BR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        Lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
 
         //тута штуки всяике тестятся
 
         ///!протестить и исправить если надо!!
 
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
-
-        //TrajectorySequence traj1 = drive.trajectorySequenceBuilder(new Pose2d());
-                //.forward(44)
-                // .strafeRight(100) //позиция поворота
-                //.strafeLeft(12)
-//                 .turn(0.135)
-//                .forward(164) //до щита
-                //1 ~ [270;320], 0.25 ~ 135, 0.18 ~ чуть больше 90, 0.15 ~ 90, 0.14 (при 12.74V) ~ 85
-                ////.addTemporalMarker(5, () -> {servobox.setPosition(0);})
-                ////.turn(90)
-//                .forward(164)
-                //.turn(0.350) //на 180
-//                .addTemporalMarker(3, () -> armRaise())
-//                .addTemporalMarker(1,() -> lohotron.setPosition(0))
-                //.addTemporalMarker(5, () -> {armRaise(); zahvat.setPosition(0);})
-                //.strafeRight(18)
-                //.forward(18)    //*/
-                //.build();
-
 
         TrajectorySequence traj1 = drive.trajectorySequenceBuilder(new Pose2d())
                 .back(33) //к линии        //проезды задаются непонятной системой мер forward - вперед, back - назад, strafeRight/Left - стрейфить
@@ -165,23 +115,6 @@ public class AutoBlueTest extends LinearOpMode{
                 //.strafeLeft(18)
                 //.turn(-90)
                 //.forward(54)
-                //.addTemporalMarker(5, () -> {servobox.setPosition(0);})
-                //.strafeRight(18)
-                //.forward(18)
-                //.build();
-
-
-
-
-        //TrajectorySequence traj3 = drive.trajectorySequenceBuilder(new Pose2d())
-                //.forward(18)
-                //.turn(90)
-                //.addTemporalMarker(5, () -> {servobox.setPosition(0);})
-                //.turn(-90)
-                //.back(2)
-                //.strafeLeft(18)
-                //.turn(-90)
-               //.forward(54)
                 //.addTemporalMarker(5, () -> {servobox.setPosition(0);})
                 //.strafeRight(18)
                 //.forward(18)
