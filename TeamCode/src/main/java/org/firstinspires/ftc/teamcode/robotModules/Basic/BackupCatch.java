08343eed2d7d36b60cd7d764a5a5ba10d2a3f5a4
nelturion <aandreyuk2001@gmail.com>
2024-02-17 04:08:37 +0300

    [2]succesfully refactored and documented opmodes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotModules/Basic/BackupCatch.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode.robotModules.Basic;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Servo;
+
+/**
+ * В этом классе описывается захват для фиолетового пикселя
+ */
+
+public class BackupCatch {
+    private LinearOpMode backOpMode;
+    private boolean grabbed = false;
+    private Servo left /*, right*/;
+
+    /**
+     * Создаем задний захват, как класс внутри opMod-а
+     * @param opMode обычно "this", задает в каком потоке оперирует наш з-захват
+     */
+
+    public BackupCatch(LinearOpMode opMode){
+        backOpMode = opMode;
+    }
+
+    /**
+     * Инициализация з-захвата для opMode. Добавление в конфигурацию
+     */
+    public void initBack(){
+        left = backOpMode.hardwareMap.servo.get("drop1");
+        //right = backOpMode.hardwareMap.servo.get("drop2");
+    }
+
+    /**
+     * захватить захват завхатывать захват вооруженный одной сервой
+     */
+    public void grab(){
+        left.setPosition(0.6);   //TODO: подобрать значение
+        //right.setPosition(0.6);
+        grabbed = true;
+    }
+
+    /**
+     * отпустить захват
+     */
+    public void ungrab(){
+        left.setPosition(0);  //TODO: подобрать значение
+        //right.setPosition(0);
+        grabbed = false;
+    }
+
+    /**
+     * Добавляем в телеметрию ключевые данные о механизме
+     */
+    public void telemetryBack(){
+        backOpMode.telemetry.addData("grabbed: ", grabbed);
+    }
+}
