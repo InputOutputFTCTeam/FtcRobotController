e96d5fb5b41755d9f9f4f4fd74ed5ba77b134e0e
nelturion <aandreyuk2001@gmail.com>
2024-02-26 03:48:26 +0300

    corrected IMUDriveTrain documentation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotModules/Sensored/IMUDriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotModules/Sensored/IMUDriveTrain.java
@@ -20,39 +20,43 @@ import org.firstinspires.ftc.teamcode.robotModules.Basic.BasicDriveTrain;
  */
 public class IMUDriveTrain extends BasicDriveTrain {
     BNO055IMUNew imu = null;
-    double headingError  = 0;
-    static final double     P_TURN_GAIN            = 0.01;
-    static final double     P_DRIVE_GAIN           = 0.03;
-    private double  targetHeading = 0;
-    private double  driveSpeed    = 0;
-    private double  turnSpeed     = 0;
-    private double  leftSpeed     = 0;
-    private double  rightSpeed    = 0;
-    private int     leftTarget    = 0;
-    private int     rightTarget   = 0;
+    double headingError = 0;
+    static final double P_TURN_GAIN = 0.01;
+    static final double P_DRIVE_GAIN = 0.03;
+    private double targetHeading = 0;
+    private double driveSpeed = 0;
+    private double turnSpeed = 0;
+    private double leftSpeed = 0;
+    private double rightSpeed = 0;
+    private int leftTarget = 0;
+    private int rightTarget = 0;
 
-    static final double     HEADING_THRESHOLD       = 1.0 ;
-    static final double     SPEED_THRESHOLD         = 0.2 ;
-    static final double     COUNTS_PER_MOTOR_REV    = 1440 ;
-    static final double     DRIVE_GEAR_REDUCTION    = 1.0 ;
-    static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;
+    static final double HEADING_THRESHOLD = 1.0;
+    static final double SPEED_THRESHOLD = 0.2;
+    static final double COUNTS_PER_MOTOR_REV = 1440;
+    static final double DRIVE_GEAR_REDUCTION = 1.0;
+    static final double WHEEL_DIAMETER_INCHES = 4.0;
 
     /**
      * Настроиваем ориентацию Контрол/Экспеншн хаба в пространстве
      */
     RevHubOrientationOnRobot.LogoFacingDirection logoDirection = RevHubOrientationOnRobot.LogoFacingDirection.RIGHT;       //направление того, как установлен Rev Hub (logo)
-    RevHubOrientationOnRobot.UsbFacingDirection  usbDirection  = RevHubOrientationOnRobot.UsbFacingDirection.DOWN;   //направление того, как установлен Rev Hub (USB)
+    RevHubOrientationOnRobot.UsbFacingDirection usbDirection = RevHubOrientationOnRobot.UsbFacingDirection.DOWN;   //направление того, как установлен Rev Hub (USB)
     RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(logoDirection, usbDirection);
 
 
-    public IMUDriveTrain() {}
-     /** Создаём КБ, как класс внутри opMode */
-    public IMUDriveTrain(LinearOpMode gyroOpMode){
+    public IMUDriveTrain() {
+    }
+
+    /**
+     * Создаём КБ, как класс внутри opMode
+     */
+    public IMUDriveTrain(LinearOpMode gyroOpMode) {
         setOpMode(gyroOpMode);
     }
 
     /**
-     * инициалируем КБ для opMode,настроиваем гироскоп
+     * Инициалируем КБ для opMode,настроиваем гироскоп
      */
     public void initIDT() {
         initMotors();
@@ -65,10 +69,15 @@ public class IMUDriveTrain extends BasicDriveTrain {
         imu.resetYaw();
     }
 
-    /**  метод для поворота на n гразусов с n-й скоростью */
+    /**
+     * Метод для поворота на heading гразусов с n-й скоростью
+     *
+     * @param maxTurnSpeed максимальная корость поворота
+     * @param heading      новое направление
+     */
     public void turnToHeading(double maxTurnSpeed, double heading) {
 
-        getSteeringCorrection(heading, P_TURN_GAIN);
+        getSteeringCorrection(heading, P_TURN_GAIN);    //
 
         while (getOpMode().opModeIsActive() && (Math.abs(headingError) > HEADING_THRESHOLD)) {
 
@@ -80,7 +89,7 @@ public class IMUDriveTrain extends BasicDriveTrain {
 
             // Pivot in place by applying the turning correction
             //moveRobot(0, turnSpeed);
-            moveRobot(0,turnSpeed);
+            moveRobot(0, turnSpeed);
 
             // Display drive status for the driver.
             //sendTelemetry(false);
@@ -96,9 +105,12 @@ public class IMUDriveTrain extends BasicDriveTrain {
 
         // Stop all motion;
         //moveRobot(0, 0);
-        move(0,0,0);
+        move(0, 0, 0);
     }
-/** метод для коррекции отклонения от целеого значения */
+
+    /**
+     * Метод для коррекции отклонения от целевого значения
+     */
     public double getSteeringCorrection(double desiredHeading, double proportionalGain) {       //П-регулируемый поворот
         targetHeading = desiredHeading;  // Save for telemetry
 
@@ -106,44 +118,45 @@ public class IMUDriveTrain extends BasicDriveTrain {
         headingError = Math.abs(Math.abs(targetHeading) - Math.abs(getHeading()));
 
         // Normalize the error to be within +/- 180 degrees
-        while (headingError > 180)  headingError -= 360;
+        while (headingError > 180) headingError -= 360;
         while (headingError <= -180) headingError += 360;
 
         // Multiply the error by the gain to determine the required steering correction/  Limit the result to +/- 1.0
         return Range.clip(headingError * proportionalGain, -1, 1);
     }
-/**
- * метод для передвижения робота в пространстве
- */
+
+    /**
+     * Метод для передвижения робота в пространстве
+     */
     public void moveRobot(double drive, double turn) {
         driveSpeed = drive;
-        turnSpeed  = turn;
+        turnSpeed = turn;
 
-        leftSpeed  = drive - turn;
+        leftSpeed = drive - turn;
         rightSpeed = drive + turn;
 
         double max = Math.max(Math.abs(leftSpeed), Math.abs(rightSpeed));
-        if (max > 1.0)
-        {
+        if (max > 1.0) {
             leftSpeed /= max;
             rightSpeed /= max;
         }
 
-        move(0,0,rightSpeed - leftSpeed);
+        move(0, 0, rightSpeed - leftSpeed);
     }
+
     private void sendTelemetry(boolean straight) {
 
         if (straight) {
             telemetry.addData("Motion", "Drive Straight");
-            telemetry.addData("Target Pos Left:Right",  "%7d:%7d",      leftTarget,  rightTarget);
-            telemetry.addData("Actual Pos Left:Right",  "%7d:%7d",      getTL().getCurrentPosition(),
+            telemetry.addData("Target Pos Left:Right", "%7d:%7d", leftTarget, rightTarget);
+            telemetry.addData("Actual Pos Left:Right", "%7d:%7d", getTL().getCurrentPosition(),
                     getTR().getCurrentPosition(), getBL().getCurrentPosition(), getBR().getCurrentPosition());
         } else {
             telemetry.addData("Motion", "Turning");
         }
 
         telemetry.addData("Heading- Target : Current", "%5.2f : %5.0f", targetHeading, getHeading());
-        telemetry.addData("Error  : Steer Pwr",  "%5.1f : %5.1f", headingError, turnSpeed);
+        telemetry.addData("Error  : Steer Pwr", "%5.1f : %5.1f", headingError, turnSpeed);
         telemetry.addData("Wheel Speeds L : R", "%5.2f : %5.2f", leftSpeed, rightSpeed);
         telemetry.update();
     }
