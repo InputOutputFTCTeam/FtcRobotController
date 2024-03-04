c4b5be37a5b2401c4099d66948479c1cd1a2bf97
nelturion <aandreyuk2001@gmail.com>
2024-03-04 19:37:52 +0300

    Убрали потоки

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/TeleOperatingMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/TeleOperatingMode.java
@@ -10,6 +10,7 @@ import org.firstinspires.ftc.teamcode.robotModules.Basic.Catch;
 import org.firstinspires.ftc.teamcode.robotModules.Basic.BasicDriveTrain;
 import org.firstinspires.ftc.teamcode.robotModules.Basic.Box;
 import org.firstinspires.ftc.teamcode.robotModules.Basic.Hook;
+import org.firstinspires.ftc.teamcode.robotModules.Basic.HookMotor;
 import org.firstinspires.ftc.teamcode.robotModules.Basic.Intaker;
 import org.firstinspires.ftc.teamcode.robotModules.Basic.Lift;
 import org.firstinspires.ftc.teamcode.robotModules.Basic.Lohotron;
@@ -17,7 +18,7 @@ import org.firstinspires.ftc.teamcode.robotModules.Basic.Plane;
 
 @TeleOp(name = "Tele V8", group = "1alfa")
 public class TeleOperatingMode extends LinearOpMode {
-    Thread movement, raiseaArm, midlower, closeClaw, openClaw, intakethread, closehook, midhook, openhook, liftThread, grab, ungrab, pushUp, agelUp, agelDown;
+    //Thread movement, raiseaArm, midlower, closeClaw, openClaw, intakethread, closehook, midhook, openhook, liftThread, grab, ungrab, pushUp, agelUp;
     BasicDriveTrain wheelbase = new BasicDriveTrain(this);
     //Intaker intake = new Intaker(this);
     Lohotron lohotron = new Lohotron(this);
@@ -26,11 +27,12 @@ public class TeleOperatingMode extends LinearOpMode {
     Catch aCatch = new Catch(this);
     Hook hook = new Hook(this);
     Plane plane = new Plane(this);
+    HookMotor hookMotor = new HookMotor(this);
 
     double INTAKE_SPEED = -0.4;
 
     @Override
-    public void runOpMode(){
+    public void runOpMode() {
 
         wheelbase.initMotors();
         wheelbase.setModes(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -48,6 +50,9 @@ public class TeleOperatingMode extends LinearOpMode {
         lift.liftDirection(DcMotorSimple.Direction.FORWARD);
         lift.liftMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
+        hookMotor.initHookMotor();
+
+
         aCatch.initCatch();
 
         plane.initPlane();
@@ -56,7 +61,7 @@ public class TeleOperatingMode extends LinearOpMode {
 
         telemetry.update();
 
-        while(!isStarted()){
+        while (!isStarted()) {
             wheelbase.wheelbaseTelemetry();
             lohotron.lohotronTelemetry();
             //intake.telemetryIntaker();
@@ -70,26 +75,21 @@ public class TeleOperatingMode extends LinearOpMode {
             idle();
         }
 
-        while(opModeIsActive()){
-            movement = new Thread(() -> {
-                wheelbase.move(gamepad1.left_stick_x, -gamepad1.left_stick_y, gamepad1.right_trigger - gamepad1.left_trigger);
-            });movement.start();
+        while (opModeIsActive()) {
+            wheelbase.move(gamepad1.left_stick_x, -gamepad1.left_stick_y, gamepad1.right_trigger - gamepad1.left_trigger);
+            if (gamepad1.left_bumper) wheelbase.setMaximumSpeed(0.5);
+            if (gamepad1.right_bumper) wheelbase.setMaximumSpeed(1);
 
-            raiseaArm = new Thread(() -> {
-                //if(gamepad2.y) lohotron.armRaiser();    //better be 1 logical button with lowerer
-                //if(gamepad2.a) lohotron.armLowerer();   //better be 1 logical button with raiser
-                if(gamepad2.a) lohotron.armLogicalRaise_Lower();
-            }); raiseaArm.start();
-            midlower = new Thread(() -> {
-                if(gamepad2.y) lohotron.armLogicalMid_Lower();
-            }); midlower.start();
+            hookMotor.run(gamepad1.right_stick_y);
 
-            closeClaw = new Thread(() -> {
-                if(gamepad2.x) lohotron.closeClaw();
-            }); closeClaw.start();
-            openClaw = new Thread(() -> {
-                if(gamepad2.b) lohotron.openClaw();
-            }); openClaw.start();
+            //if(gamepad2.y) lohotron.armRaiser();    //better be 1 logical button with lowerer
+            //if(gamepad2.a) lohotron.armLowerer();   //better be 1 logical button with raiser
+            if (gamepad2.a) lohotron.armLogicalRaise_Lower();
+            if (gamepad2.y) lohotron.armLogicalMid_Lower();
+
+            if (gamepad2.x) lohotron.logicalOpenCloseClaw();
+            //if (gamepad2.b) lohotron.openClaw();
+
 
             if (gamepad2.dpad_left) aCatch.openGrab();
             if (gamepad2.dpad_right) aCatch.closeGrab();
@@ -99,48 +99,37 @@ public class TeleOperatingMode extends LinearOpMode {
                 intake.runIntake((gamepad2.right_trigger - gamepad2.left_trigger) * INTAKE_SPEED);
             }); intakethread.start();
             */
-            closehook = new Thread(() -> {
-                if(gamepad1.dpad_down) hook.closeHook();
-            }); closehook.start();
-            midhook = new Thread(() -> {
-                //if(gamepad2.dpad_right) hook.midHook();
-            }); midhook.start();
-            openhook = new Thread(() -> {
-                //if(gamepad1.dpad_up) hook.openHook();
-            }); openhook.start();
-            liftThread = new Thread(() -> {
-                lift.run(gamepad2.right_stick_y);
-            }); liftThread.start();
 
-            grab = new Thread(() -> {
-                if(gamepad2.right_bumper) aCatch.grab();    //или gamepad2.right_stick_button
-            }); grab.start();
-            ungrab = new Thread(() -> {
-                if(gamepad2.left_bumper) aCatch.ungrab();       //    gamepad2.left_stick_button
-            }); ungrab.start();
+            if (gamepad1.dpad_down) hook.closeHook();
+
+            //if(gamepad2.dpad_right) hook.midHook();
+
+            //if(gamepad1.dpad_up) hook.openHook();
+
+            lift.run(gamepad2.right_stick_y);
+
+
+            if (gamepad2.right_bumper) aCatch.grab();    //или gamepad2.right_stick_button
+
+            if (gamepad2.left_bumper) aCatch.ungrab();       //    gamepad2.left_stick_button
 
             ////if(gamepad1.a) hook.switchHook();
             //if(gamepad1.dpad_up) hook.openHook();
-            pushUp = new Thread(() -> {
-                if(gamepad1.y) plane.pushUp();
-            }); pushUp.start();
 
-            agelUp = new Thread(() -> {
-                if (gamepad1.x) plane.angleUp();
-            }); agelUp.start();
+            if (gamepad1.x) plane.pushUp();
+
+
+            if (gamepad1.b) plane.angleUp();
+
             composeTelemery();
 
-            agelDown = new Thread(() -> {
-                if (gamepad1.b) plane.angleDown();
-            }); agelDown.start();
-            composeTelemery();
-
+            if (gamepad1.b) plane.angleDown();
 
 
         }
     }
 
-    public void composeTelemery(){
+    public void composeTelemery() {
         wheelbase.wheelbaseTelemetry();
         lohotron.lohotronTelemetry();
         //intake.telemetryIntaker();
