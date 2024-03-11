3e5425946cb782bbbb1862233e4122b3c934188a
vanyabuzdalov <vanyabuzdalov@yandex.ru>
2024-03-11 19:38:49 +0300

    Для написания мною автонома

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/G_blue2_1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/G_blue2_1.java
@@ -18,6 +18,10 @@ public class G_blue2_1 extends LinearOpMode {
         waitForStart();
         if (opModeIsActive()) {
             robot.imuSteerEncoder(0, 1, 0, 0, 1000);
+
+            //encoder run - проезд вперёд, imuTurn- оаорот, объявить сервы что использовать как в голуюой тест
+
+
             //sleep(5000);
             //robot.colorRun(0.5, 0, 0, ColorSensorModule.colorsField.BLUE);
         }
