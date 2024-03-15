d9d941cb4a487a9ccbe7cc571c2c8320ea7b0a41
nelturion <aandreyuk2001@gmail.com>
2024-03-15 21:59:08 +0300

    TouchSensor sdelal normalno

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotModules/Sensors/TouchSensor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotModules/Sensors/TouchSensor.java
@@ -21,26 +21,3 @@ public class TouchSensor {
         touchSensorOpMode.telemetry.addLine(isPressed() ? "pressed" : "not pressed");
     }
 }
-package org.firstinspires.ftc.teamcode.robotModules.Sensors;
-
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
-public class TouchSensor {
-    LinearOpMode touchSensorOpMode;
-    TouchSensor ts;
-    public TouchSensor(LinearOpMode opMode) {
-        touchSensorOpMode = opMode;
-    }
-
-    public void initTouch() {
-        ts = touchSensorOpMode.hardwareMap.get(TouchSensor.class, "sensor_touch");
-    }
-
-    public boolean isPressed() {
-        return ts.isPressed();
-    }
-
-    public void touchTelemetry() {
-        touchSensorOpMode.telemetry.addLine(isPressed() ? "pressed" : "not pressed");
-    }
-}
