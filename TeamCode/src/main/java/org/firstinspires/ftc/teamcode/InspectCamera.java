bbe0c9189f6015204024ba3b5af82f6b43302d6e
Input/Output <ftc17438@gmail.com>
2024-03-20 18:47:20 +0300

    настройка для инспекиции

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/InspectCamera.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcontroller.EOCVSamples.PhantomSamples.Methods_for_OpenCV;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvWebcam;
+
+@TeleOp
+public class InspectCamera extends LinearOpMode {
+    private static int valLeft = -1;
+    private static int valRight = -1;
+    public OpenCvWebcam phoneCam;
+
+    @Override
+    public void runOpMode() {
+        Methods_for_OpenCV methodsForOpenCV = new Methods_for_OpenCV();
+        int rows = methodsForOpenCV.getRows();
+        int cols = methodsForOpenCV.getCols();
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        phoneCam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        phoneCam.openCameraDevice();
+        phoneCam.setPipeline(new org.firstinspires.ftc.robotcontroller.EOCVSamples.PhantomSamples.Methods_for_OpenCV.StageSwitchingPipeline());
+        phoneCam.startStreaming(rows, cols, OpenCvCameraRotation.UPRIGHT);
+
+        Thread thread = new Thread(() -> {
+            while (opModeInInit()) {
+                telemetry.addData("Values", valLeft + "  " + valRight);
+                telemetry.update();
+                // visionPortall.telemetryAprilTag();
+                valLeft = Methods_for_OpenCV.getValLeft();
+                valRight = Methods_for_OpenCV.getValRight();
+            }
+        });
+        FtcDashboard.getInstance().startCameraStream(phoneCam, 210);
+        FtcDashboard.getInstance().getTelemetry();
+        thread.start();
+
+        waitForStart();
+        while (opModeIsActive()) {
+
+        }
+    }
+}
